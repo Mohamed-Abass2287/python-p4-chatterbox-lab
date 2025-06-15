611135e0baf63e4b60f609c312a7ae6bf0ee69dc
Mohamed-Abass2287 <aminabass987@gmail.com>
2025-06-15 12:04:29 +0300

    add

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 
 from models import db, Message
 
-app = Flask(__name__)
+app = Flask(__name__)  
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
@@ -14,13 +14,40 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+@app.route("/")
+def index():
+    return "<h1>Welcome to my Chatterbox</h1>"
+
 @app.route('/messages')
 def messages():
-    return ''
+    my_messages = Message.query.order_by(Message.created_at.asc()).all()
+    message_list = [message.to_dict() for message in my_messages]
 
-@app.route('/messages/<int:id>')
+    return make_response(message_list, 200)
+
+@app.route("/messages", methods=["POST"])
+def create_message():
+    data = request.get_json()
+    new_message = Message(body=data['body'], username=data['username'])
+    db.session.add(new_message)
+    db.session.commit()
+    return make_response(new_message.to_dict(), 200)
+
+@app.route('/messages/<int:id>', methods=["PATCH"])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+    data = request.get_json()
+    message.body = data['body']
+    db.session.commit()
+    return make_response(message.to_dict(), 200)
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.filter_by(id=id).first()
+    db.session.delete(message)
+    db.session.commit()
+    return make_response({"message": "Message deleted successfully"}, 200)
+
 
 if __name__ == '__main__':
     app.run(port=5555)
